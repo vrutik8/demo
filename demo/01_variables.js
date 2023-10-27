db771d518e498014a7f52e99c1a49e5cff476d1d
vrutik <surtivrutik18@gmail.com>
2023-10-27 10:49:57 +0530

    demo2

---
+++ demo/01_variables.js
@@ -0,0 +1,25 @@
+// const can be declared and defined once
+const accountId = 14454
+
+// let can be changed it can be declared and defined later also for let
+let accountEmail = "surtivrutik18@gmail.com";
+let accountState ;
+
+
+// var shouldn't be used because of issue in functional and block scope
+var accountPassword = "1234"
+
+// not preferred
+accountCity = "jaipur";
+
+// accountId = 2
+
+accountEmail = "sur@123.com"
+accountPassword ="11111"
+accountCity="delhi";
+
+
+// to show all variable once instead of console use console.table([])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
